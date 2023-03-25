99f71503915b5acef2f206455ee5e5f785390f71
OLEVANDR55 <83229879+OLEVANDR55@users.noreply.github.com>
2023-03-25 21:01:14 +0300

    Fix bug

--- TestUi/Assets/Scripts/ButtonScript.cs
+++ TestUi/Assets/Scripts/ButtonScript.cs
@@ -2,7 +2,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
-{
+{ 
     public void Scenes(int NumScene)
     {
         SceneManager.LoadScene(NumScene);
